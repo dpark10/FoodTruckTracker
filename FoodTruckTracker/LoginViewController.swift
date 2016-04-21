ca1b1831ebff0ca6e7972b86d0a4ec68ba775774
Dan Park <parkds@gmail.com>
2016-04-21 11:52:54 -0500

    firebase login

--- FoodTruckTracker/LoginViewController.swift
+++ FoodTruckTracker/LoginViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    
+    @IBAction func onLoginButtonTapped(sender: UIButton) {
+        let email = emailAddressTextField.text
+        let password = passwordTextField.text
+        
+        guard email?.characters.count != 0 else { return }
+        guard password?.characters.count != 0 else { return }
+        self.loginUser(email!, password: password!)
+    }
+    
+    func loginUser(email: String, password: String) {
+        let ref = DataService.dataService.REF_BASE
+        ref.authUser(email, password: password) { (error, authData) in
+            if error != nil {
+                print(error)
+            } else {
+                self.performSegueWithIdentifier("ToMainScreen", sender: nil)
+            }
+        }
         
         
     }
---
+++ FoodTruckTracker/SignupViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SignupViewController.swift
+//  FoodTruckTracker
+//
+//  Created by dp on 4/21/16.
+//  Copyright © 2016 Dan Park. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func onCreateButtonTapped(sender: UIButton) {
+        let email = emailTextField.text
+        let password = passwordTextField.text
+        
+        guard email?.characters.count != 0 else { return }
+        guard password?.characters.count != 0 else { return }
+        self.newUserCreation(email!, password: password!)
+    }
+    
+    func newUserCreation (email: String, password: String) {
+        let ref = DataService.dataService.REF_BASE
+        ref.createUser(email, password: password) { (error, result) in
+            if error != nil {
+                print(error)
+            } else {
+                let uid = result["uid"] as? String
+                print("Successfully created user account with uid: \(uid)")
+            }
+        }
+    }
+
+}
