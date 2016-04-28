2fbe821b8c7e79fe5373f6c6ac160887b37b5cd7
Dan Park <parkds@gmail.com>
2016-04-28 14:04:30 -0500

    replace ScannerViewController with QRScannerViewController

---
+++ FoodTruckTracker/QRScannerViewController.swift
@@ -0,0 +1,103 @@
+//
+//  QRScannerViewController.swift
+//
+//
+//  Created by dp on 4/27/16.
+//  Copyright © 2016 Dan Park. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    @IBOutlet weak var qrCodeLabel: UILabel!
+    @IBOutlet weak var qrCodeResultLabel: UILabel!
+    
+    var objCaptureSession: AVCaptureSession?
+    var objCaptureVideoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var vwQRCode: UIView?
+    var couponString = "FoodTruckCouponTest"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.configureVideoCapture()
+        self.addVideoPreviewLayer()
+        self.initializeQRView()
+        
+    }
+    
+    func configureVideoCapture() {
+        let objCaptureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        var error: NSError?
+        let objCaptureDeviceInput: AnyObject!
+        
+        do {
+            objCaptureDeviceInput = try AVCaptureDeviceInput(device: objCaptureDevice) as AVCaptureDeviceInput
+        } catch let error1 as NSError {
+            error = error1
+            objCaptureDeviceInput = nil
+        }
+        
+        if (error != nil) {
+            let alertController:UIAlertController = UIAlertController(title: "Device Error", message: "Device not supported", preferredStyle: .Alert)
+            let cancelAction:UIAlertAction = UIAlertAction(title: "OK", style: .Cancel, handler: { (alertAction) -> Void in
+                alertController.dismissViewControllerAnimated(true, completion: nil)
+            })
+            
+            alertController.addAction(cancelAction)
+            self.presentViewController(alertController, animated: true, completion: nil)
+            return
+        }
+        
+        objCaptureSession = AVCaptureSession()
+        objCaptureSession?.addInput(objCaptureDeviceInput as! AVCaptureInput)
+        let objCaptureMetadataOutput = AVCaptureMetadataOutput()
+        objCaptureSession?.addOutput(objCaptureMetadataOutput)
+        objCaptureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        objCaptureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+    }
+    
+    func addVideoPreviewLayer() {
+        objCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: objCaptureSession)
+        objCaptureVideoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        objCaptureVideoPreviewLayer?.frame = view.layer.bounds
+        self.view.layer.addSublayer(objCaptureVideoPreviewLayer!)
+        objCaptureSession?.startRunning()
+        
+        self.view.bringSubviewToFront(qrCodeLabel)
+        self.view.bringSubviewToFront(qrCodeResultLabel)
+    }
+    
+    
+    func initializeQRView() {
+        vwQRCode = UIView()
+        vwQRCode?.layer.borderColor = UIColor.redColor().CGColor
+        vwQRCode?.layer.borderWidth = 5
+        self.view.addSubview(vwQRCode!)
+        self.view.bringSubviewToFront(vwQRCode!)
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            vwQRCode?.frame = CGRectZero
+            qrCodeResultLabel.text = "NO QR Code text detected"
+            return
+        }
+        let objMetadataMachineReadableCodeObject = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        if objMetadataMachineReadableCodeObject.type == AVMetadataObjectTypeQRCode {
+            let objBarCode = objCaptureVideoPreviewLayer?.transformedMetadataObjectForMetadataObject(objMetadataMachineReadableCodeObject as AVMetadataMachineReadableCodeObject) as! AVMetadataMachineReadableCodeObject
+            vwQRCode?.frame = objBarCode.bounds;
+            if objMetadataMachineReadableCodeObject.stringValue != nil {
+                qrCodeResultLabel.text = objMetadataMachineReadableCodeObject.stringValue
+                if qrCodeResultLabel.text == couponString {
+                    AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+                    vwQRCode?.layer.borderColor = UIColor.greenColor().CGColor
+                    objCaptureSession?.stopRunning()
+                }
+            }
+        }
+    }
+}
