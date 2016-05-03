8ba31c69a49194652745f9d316adf3a652fd30ad
Dan Park <parkds@gmail.com>
2016-05-03 10:56:49 -0500

    food truck owner profile vc added, scrolling and keyboard fixed

---
+++ FoodTruckTracker/CouponViewController.swift
@@ -0,0 +1,172 @@
+//
+//  CouponViewController.swift
+//  FoodTruckTracker
+//
+//  Created by dp on 5/2/16.
+//  Copyright © 2016 Dan Park. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class CouponViewController: UIViewController, UIScrollViewDelegate, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    // MARK: IBOutlets
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var categoryTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var websiteTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var twitterTextField: UITextField!
+    @IBOutlet weak var yelpIDTextField: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var couponDescTextField: UITextField!
+    @IBOutlet weak var discountAmtTextField: UITextField!
+    @IBOutlet weak var expDateTextField: UITextField!
+    
+    
+    // MARK: Properties
+    var locationManager = CLLocationManager()
+    var userLocation = CLLocationCoordinate2D()
+    var activeField: UITextField?
+    
+    
+    // MARK: View Management
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.delegate = self
+        scrollView.addSubview(contentView)
+        scrollView.contentSize = CGSize(width:340, height:900)
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        mapView.delegate = self
+        
+        self.registerForKeyboardNotifications()
+
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        self.deregisterFromKeyboardNotifications()
+    }
+
+    
+    func registerForKeyboardNotifications()
+    {
+        //Adding notifies on keyboard appearing
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(CouponViewController.keyboardWasShown(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(CouponViewController.keyboardWillBeHidden(_:)), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    
+    func deregisterFromKeyboardNotifications()
+    {
+        //Removing notifies on keyboard appearing
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWasShown(notification: NSNotification)
+    {
+        //Need to calculate keyboard exact size due to Apple suggestions
+        self.scrollView.scrollEnabled = true
+        let info : NSDictionary = notification.userInfo!
+        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue().size
+        let contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize!.height, 0.0)
+        
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+        
+        var aRect : CGRect = self.view.frame
+        aRect.size.height -= keyboardSize!.height
+        if let activeFieldPresent = activeField
+        {
+            if (!CGRectContainsPoint(aRect, activeField!.frame.origin))
+            {
+                self.scrollView.scrollRectToVisible(activeField!.frame, animated: true)
+            }
+        }
+        
+        
+    }
+    
+    
+    func keyboardWillBeHidden(notification: NSNotification)
+    {
+        //Once keyboard disappears, restore original positions
+        let info : NSDictionary = notification.userInfo!
+        let keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue().size
+        let contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, -keyboardSize!.height, 0.0)
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+        self.view.endEditing(true)
+        self.scrollView.contentInset = UIEdgeInsetsZero
+        
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField!)
+    {
+        activeField = textField
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField!)
+    {
+        activeField = nil
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.contentSize = CGSize(width:340, height:900)
+    }
+    
+    
+    // MARK: IBActions
+    
+    @IBAction func setFoodTruckLocation(sender: UIButton) {
+        print("Button Tapped")
+
+    }
+    
+    @IBAction func addImageButtonTapped(sender: AnyObject) {
+        print("Button Tapped")
+        
+    }
+    
+    @IBAction func hideKeyboard(sender: AnyObject) {
+        nameTextField.endEditing(true)
+        categoryTextField.endEditing(true)
+        phoneTextField.endEditing(true)
+        websiteTextField.endEditing(true)
+        emailTextField.endEditing(true)
+        twitterTextField.endEditing(true)
+        yelpIDTextField.endEditing(true)
+        couponDescTextField.endEditing(true)
+        discountAmtTextField.endEditing(true)
+        expDateTextField.endEditing(true)
+    }
+    
+    
+    
+    // MARK: CLLocationManagerDelegate Methods
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error)
+    }
+    
+    func mapView(mapView: MKMapView, didUpdateUserLocation userLocation: MKUserLocation) {
+        locationManager.stopUpdatingLocation()
+        let userLocation = mapView.userLocation
+        let region = MKCoordinateRegionMakeWithDistance(userLocation.location!.coordinate, 5000, 5000)
+        mapView.setRegion(region, animated: true)
+        self.userLocation = userLocation.location!.coordinate
+        print(self.userLocation)
+    }
+
+}
